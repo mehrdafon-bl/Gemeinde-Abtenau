8b642e1cd7697d8f9dcadff2ccbc35dae2ac7fbc
Christofer Huber <ch@webpunks.co>
2025-02-21 21:47:42 +0700

    Updated to gradle 8.7.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { LiveupdateService } from './liveupdate.service';
-import { Device } from '@capacitor/device';
 import { OneSignalService } from './onesignal.service';
 
 @Component({
@@ -71,26 +70,5 @@ export class AppComponent implements OnInit {
     } else {
       this.menuStateClass = 'close';
     }
-  }
-
-  async deviceData() {
-    // if (this.deviceDataTabCounter >= 4 && this.showDeviceData === false) {
-    //     this.showDeviceData = true;
-    //     this.oneSignal.getIds().then(data => {
-    //         console.log('🚀 ~ AppComponent ~ this.oneSignal.getIds ~ data:', data)
-    //         this.pushId = data.userId;
-    //     });
-    //     OneSignal.
-    // this.deviceId = await Device.getId();
-    //     console.log('🚀 ~ AppComponent ~ deviceData ~ this.deviceId:', this.deviceId)
-    //     setTimeout(
-    //         () => {
-    //             this.showDeviceData = false;
-    //             this.deviceDataTabCounter = 0;
-    //         }, 15000
-    //     );
-    // } else {
-    //     this.deviceDataTabCounter++;
-    // }
   }
 }
